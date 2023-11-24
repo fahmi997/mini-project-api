89a7405cb28fb0645a16bb7cebd8d1fdb70e25dc
Emma0123 <khansakamila467@gmail.com>
2023-11-24 10:47:06 +0700

    fix: index.js controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 // const controller_name = require('./controller_name');
 const eventController = require('./event')
 const ticketController = require('./ticket');
+const cityController = require('./city');
 
 module.exports = {
     eventController,
     ticketController,
+    cityController
 };
