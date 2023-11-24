fb36a3cbb451892a64993978d75bbbd92cf94abd
Emma0123 <khansakamila467@gmail.com>
2023-11-24 09:44:17 +0700

    feat: event by id

--- controllers/event.js
+++ controllers/event.js
@@ -2,71 +2,91 @@ const { errorRespose } = require("../helper/utils");
 const { categories, provinces, cities, events } = require("../models");
 
 module.exports = {
-    getAllEvents: async (req, res, next) => {
-        try {
-            const result = await events.findAll();
-            return res.status(200).send(result);
-        } catch (error) {
-            console.log(error);
-            next(error);
-        }
-    },
-    create: async (req, res, next) => {
-        try {
-            const result = await service_name.create(req.body);
-            res.status(200).json(result);
-        } catch (error) {
-            next(error);
-        }
-    },
+  getAllEvents: async (req, res, next) => {
+    try {
+      const result = await events.findAll();
+      return res.status(200).send(result);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  },
+  create: async (req, res, next) => {
+    try {
+      const result = await service_name.create(req.body);
+      res.status(200).json(result);
+    } catch (error) {
+      next(error);
+    }
+  },
 
-    getCategories: async (req, res, next) => {
-        try {
-            const result = await categories.findAll();
-            if (!result) {
-                return next(errorRespose(404, false, "Not Found", "Categories not found"));
-            }
-            res.status(200).json(result);
-        } catch (error) {
-            next(errorRespose(500, false, error.message, error.stack));
-        }
-    },
+  getCategories: async (req, res, next) => {
+    try {
+      const result = await categories.findAll();
+      if (!result) {
+        return next(
+          errorRespose(404, false, "Not Found", "Categories not found")
+        );
+      }
+      res.status(200).json(result);
+    } catch (error) {
+      next(errorRespose(500, false, error.message, error.stack));
+    }
+  },
 
-    getProvinces: async (req, res, next) => {
-        try {
-            const result = await provinces.findAll();
-            if (!result) {
-                return next(errorRespose(404, false, "Not Found", "Categories not found"));
-            }
-            res.status(200).json(result);
-        } catch (error) {
-            next(errorRespose(500, false, error.message, error.stack));
-        }
-    },
+  getProvinces: async (req, res, next) => {
+    try {
+      const result = await provinces.findAll();
+      if (!result) {
+        return next(
+          errorRespose(404, false, "Not Found", "Categories not found")
+        );
+      }
+      res.status(200).json(result);
+    } catch (error) {
+      next(errorRespose(500, false, error.message, error.stack));
+    }
+  },
 
-    getCities: async (req, res, next) => {
-        try {
-            const result = await cities.findAll(
-                {
-                    where: {
-                        provId: req.params.id
-                    },
-                    include: [
-                        {
-                            model: provinces,
-                            as: 'province',
-                            attributes: ['name']
-                        }
-                    ]
-                }
-            );
-            
-            if (!result) {
-                return next(errorRespose(404, false, "Not Found", "Categories not found"));
-            }
-            res.status(200).json(result);
-        } catch (error) {
-            next(errorRespose(500, false, error.message, error.stack));
-        }
-    },
+  getCities: async (req, res, next) => {
+    try {
+      const result = await cities.findAll({
+        where: {
+          provId: req.params.id,
+        },
+        include: [
+          {
+            model: provinces,
+            as: "province",
+            attributes: ["name"],
+          },
+        ],
+      });
+
+      if (!result) {
+        return next(
+          errorRespose(404, false, "Not Found", "Categories not found")
+        );
+      }
+      res.status(200).json(result);
+    } catch (error) {
+      next(errorRespose(500, false, error.message, error.stack));
+    }
+  },
+
+  getEventById: async (req, res, next) => {
+    const { id } = req.params;
+
+    try {
+      const eventData = await events.findByPk(id);
+
+      if (!eventData) {
+        return res.status(404).json({ message: "Event not found" });
+      }
+
+      res.status(200).json(eventData);
+    } catch (error) {
+        next(errorRespose(500, false, error.message, error.stack));
+    }
+  },
 };
--- routers/event.js
+++ routers/event.js
@@ -6,5 +6,6 @@ router.get('/', eventController.getAllEvents)
 router.get('/categories', eventController.getCategories)
 router.get('/provinces', eventController.getProvinces)
 router.get('/cities/:id', eventController.getCities)
+router.get('/:id', eventController.getEventById)
 
 module.exports = router;
