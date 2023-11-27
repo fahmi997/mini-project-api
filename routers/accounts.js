ef4c71cd87e6eee63815e1f04a3427118d6145d2
fahmi997 <aardiansyah.fahmii@gmail.com>
2023-11-27 20:01:54 +0700

    fix: bug fix

--- routers/accounts.js
+++ routers/accounts.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { accountsController } = require("../controllers");
 const { uploader } = require("../helper/uploader");
-const { validateToken, validateAccount } = require("../middleware/validation");
+const { validateToken, validateAccount } = require("../middlewares/validation");
 router.post("/login", accountsController.loginAcc);
 router.post("/register", accountsController.create);
 router.get("/keeplogin", validateToken, accountsController.keepLogin);
